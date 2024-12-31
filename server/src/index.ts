7ac831601354f08308dc8a9e7dd80491c8369068
Pranav Mitan <pranavmitan@Pranavs-MacBook-Air.local>
2025-01-01 00:02:36 +0530

    fix mongodb issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,13 +9,19 @@ import { videoRouter } from './routes/videos.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5000', 'https://youtube-shorts-player.onrender.com']
+}));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI!)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI!, {
+  serverSelectionTimeoutMS: 30000, // 30 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+  connectTimeoutMS: 30000, // 30 seconds
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/videos', videoRouter);
